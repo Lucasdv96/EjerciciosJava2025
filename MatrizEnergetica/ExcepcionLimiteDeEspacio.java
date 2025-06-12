51f00526cacb228c080c98bf32b255509229869f
Lucasdv96 <ludelvalle@uade.edu.ar>
2025-06-12 00:03:29 -0300

    Actualizacion

---
+++ MatrizEnergetica/ExcepcionLimiteDeEspacio.java
@@ -0,0 +1,5 @@
+public class ExcepcionLimiteDeEspacio extends RuntimeException {
+    public ExcepcionLimiteDeEspacio(String message) {
+        super(message);
+    }
+}
--- MatrizEnergetica/src/CentralNuclear.java
+++ MatrizEnergetica/src/CentralNuclear.java
@@ -1,2 +1,8 @@
+import java.util.HashSet;
+
 public class CentralNuclear extends CentralEnergetica{
+    int limite = 5;
+
+    HashSet<ReactorNuclear> reactorNuclears = new HashSet<>();
+
 }
