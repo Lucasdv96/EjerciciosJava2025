cd46cd75193df1222455e1d81617c225d923e0dd
Lucasdv96 <ludelvalle@uade.edu.ar>
2025-07-09 19:13:12 -0300

    Update PapaNoel.java

--- PapaNoelDeMascotas/src/PapaNoel.java
+++ PapaNoelDeMascotas/src/PapaNoel.java
@@ -3,5 +3,6 @@ import java.util.HashSet;
 public class PapaNoel {
     HashSet<Domicilio> domicilios = new HashSet<>();
     HashSet<Domicilio> domiciliosVisitados = new HashSet<>();
+    HashSet<Domicilio> domiciliosPendientes = new HashSet<>();
     HashSet<Domicilio> domiciliosBloqueados = new HashSet<>();
 }
